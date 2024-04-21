feb54b13944a19485505a0114d53ed5e0bdaa850
Ajani13Brown <166076720+Ajani13Brown@users.noreply.github.com>
2024-04-20 21:55:54 -0400

    Add files via upload

--- Q1_Product_Review_Analysis.py
+++ Q1_Product_Review_Analysis.py
@@ -24,17 +24,17 @@
 python_reviews = [ "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "Poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it." ]
 
         
-# def keyword_check():
-#     python_reviews = [ "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "Poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it." ]
-#     keywords = ['good','excellent','bad','poor','average']
-#     for review in python_reviews:
-#         for word in keywords:
-#             if word in review.lower():
-#                 keyword_reviews = review.lower().replace(word,word.upper())
-                #print(keyword_reviews)
+def keyword_check():
+    python_reviews = [ "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "Poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it." ]
+    keywords = ['good','excellent','bad','poor','average']
+    for review in python_reviews:
+        for word in keywords:
+            if word in review.lower():
+                keyword_reviews = review.lower().replace(word,word.upper())
+                print(keyword_reviews)
 
 
-#keyword_check()
+keyword_check()
 
 #Task 2: Sentiment Tally
 #Develop a function that tallies the number of positive and negative words in each review. Use a predefined list of positive and negative
@@ -43,21 +43,18 @@ python_reviews = [ "This product is really good. I'm impressed with its quality.
 positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"]
 negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
 
-# def pos_and_neg_count():
-#     for review in python_reviews:
-#         pos_total= 0
-#         neg_total =0
-#         for word in positive_words:
-#             pos_total += review.lower().count(word)
-#         for word in negative_words:
-#             neg_total += review.lower().count(word)
-#         print(pos_total, neg_total)
-#             #     if word in review.lower():
-#             # elif:
-#             #     for word in negative_words:
+def pos_and_neg_count():
+    for review in python_reviews:
+        pos_total= 0
+        neg_total =0
+        for word in positive_words:
+            pos_total += review.lower().count(word)
+        for word in negative_words:
+            neg_total += review.lower().count(word)
+        print(pos_total, neg_total)
 
-# pos_and_neg_count()
-# #pos_and_neg_count()
+pos_and_neg_count()
+
 
 # Task 3: Review Summary
 # Implement a script that takes the first 30 characters of a review and appends "…" to create a summary.
